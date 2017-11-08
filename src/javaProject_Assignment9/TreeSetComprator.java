bbed1b1f922b19632c6e7e075bfa5d589e9bf870
gunarora <gunjanarrora@gmail.com>
2017-11-08 22:12:13 +0530

    Update TreeSetComprator.java

--- src/javaProject_Assignment9/TreeSetComprator.java
+++ src/javaProject_Assignment9/TreeSetComprator.java
@@ -1,18 +1,22 @@
-package javaProject_Assignment9;
+package javaProject_Assignment9; //Package declaration is mandatory
 
-
+//Importing packages
 import java.util.Set;
 import java.util.TreeSet;
 
+
 public class TreeSetComprator {
+	//Main method started
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//Tree set -- adding values
 		Set<Employee> salComp = new TreeSet<Employee>(new SalaryComparison());
         salComp.add(new Employee("Mridul","Sr. Engg",1000));
         salComp.add(new Employee("John","Bussiness Analyst",1000));
         salComp.add(new Employee("Gunjan","Manager",6000));
         salComp.add(new Employee("Gunjan","Lead",4000));
-       
+      // loop to printing emp info as per logic
         for(Employee e:salComp){
             System.out.println(e);
         }
